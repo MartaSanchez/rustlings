b61ce0eec6f64161953e49c452cfc934cd10fcb9
Marta <marta@Salvadors-MacBook-Pro.local>
2023-03-05 21:05:23 +0100

    some exercises

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,12 +1,12 @@
 // variables5.rs
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
+    let mut number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
     number = "3"; // don't rename this variable
-    let new_number:_ = number.parse().unwrap();
+    let new_number:i32;
+    new_number =  number.parse().unwrap();
     println!("Number plus two is : {}", new_number + 2);
 }
